7d4be75a2615c1dba704c3cc6c5a394d3771fd1c
Jered <jerednav@gmail.com>
2021-09-01 21:55:56 -0700

    added navlogo, mobileicon, navmenu, navitem, nav links

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,5 +1,6 @@
 import React from 'react'
-import { Nav, NavbarContainer, NavLogo } from './NavbarElements'
+import { FaBars } from 'react-icons/fa'
+import { Nav, NavbarContainer, NavLogo, MobileIcon, NavMenu, NavItem, NavLinks } from './NavbarElements'
 
 
 const Navbar = () => {
@@ -8,8 +9,17 @@ const Navbar = () => {
         <Nav>
             <NavbarContainer>
                 <NavLogo to='/'>
-                    mindful.
+                    jerednav
                 </NavLogo>
+                <MobileIcon>
+                    <FaBars />
+                </MobileIcon>
+                <NavMenu>
+                    <NavItem>
+                        <NavLinks to="about">About</NavLinks>
+                    </NavItem>
+                </NavMenu>
+
             </NavbarContainer>
         </Nav>
 
