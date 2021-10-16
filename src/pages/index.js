4ab66ffc0b601f1a249038fe2b1ee89fe5b9768a
jerednav <80506833+jerednav@users.noreply.github.com>
2021-10-16 02:43:52 -0700

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,7 +2,7 @@ import React, {useState} from 'react'
 import Navbar from '../components/Navbar'
 import Sidebar from '../components/Sidebar'
 import InfoSection from '../components/InfoSection'
-import Footer from '../components/Footer'
+// import Footer from '../components/Footer'
 
 
 
